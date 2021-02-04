809f6ad78a99929db317c3f7b67ae1f5bbfa8730
oxmagdi <miggzat@gmail.com>
2021-02-04 02:07:34 +0200

    categories sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,30 +18,24 @@
                 <!-- Blog Categories Well -->
                 <div class="well">
                     <h4>Blog Categories</h4>
+                    <?php 
+
+                        $query = "SELECT * FROM categories";
+                        $all_categories_query_result = mysqli_query($connection, $query);
+                    ?>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php 
+
+
+                        while($row = mysqli_fetch_assoc($all_categories_query_result)){
+                            
+                            $cat_title = $row['cat_title'];
+                            echo "<li><a href='#''>{$cat_title}</a></li>";
+                        }
+
+                                ?>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
@@ -50,9 +44,6 @@
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "widget.php"; ?>
 
             </div>
